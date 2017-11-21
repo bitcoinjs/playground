cace975b5d8e16eca705e79c6334e7c244f426e6
Daniel Cousens <github@dcousens.com>
2017-11-21 12:21:39 +1100

    add p2pk

--- scripts.js
+++ scripts.js
@@ -57,12 +57,65 @@ function stacksEqual (a, b) {
   return true
 }
 
-function p2ms () {
+// input: OP_0 [signatures ...]
+// output: m [pubKeys ...] n OP_CHECKMULTISIG
+function p2ms (a) {
 
 }
 
-function p2pk () {
+// input: {signature}
+// output: {pubKey} OP_CHECKSIG
+function p2pk (a) {
+  typef({
+    input: typef.maybe(typef.Buffer),
+    network: typef.maybe(typef.Object),
+    output: typef.maybe(typef.Buffer),
+    pubkey: typef.maybe(bscript.isCanonicalPubKey),
+    signature: typef.maybe(bscript.isCanonicalSignature)
+  }, a)
 
+  let input = a.input
+  let signature = a.signature
+
+  if (signature) {
+    let script = bscript.compile([signature])
+    if (input && !input.equals(script)) throw new TypeError('Input mismatch')
+    if (!input) input = script
+
+  // use input for data
+  } else if (input) {
+    let chunks = bscript.decompile(input)
+    if (chunks.length !== 1 ||
+      !bscript.isCanonicalSignature(chunks[0])) throw new TypeError('Input is invalid')
+
+    if (signature && !signature.equals(chunks[0])) throw new TypeError('Signature mismatch')
+    if (!signature) signature = chunks[0]
+  }
+
+  let network = a.network || bnetworks.bitcoin
+  let output = a.output
+  let pubkey = a.pubkey
+
+  if (output) {
+    if (output[output.length - 1] !== OPS.CHECKSIG) throw new TypeError('Output is invalid')
+
+    let outputPubKey = output.slice(0, -1)
+    if (pubkey && !pubkey.equals(outputPubKey)) throw new TypeError('PubKey mismatch')
+    if (!pubkey) pubkey = outputPubKey
+  }
+
+  if (!pubkey) throw new TypeError('Not enough data')
+  if (!output) {
+    output = bscript.compile([
+      OPS.OP_CHECKSIG,
+      pubkey
+    ])
+  }
+
+  let result = { network, output, pubkey }
+  if (input) result.input = input
+  if (signature) result.signature = signature
+  return result
 }
 
 // input: {signature} {pubkey}
